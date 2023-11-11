6979aa6a4172185946312faf636afa7b7d9133c5
Kevin7744 <arapbiisubmisions@gmail.com>
2023-11-11 11:30:22 +0300

    User interface

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,22 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+from .models import ToDoList, ToDoItem
 from django.shortcuts import render
 
-# Create your views here.
+
+class ListListView(ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "todo/app/todo_list.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+    
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
